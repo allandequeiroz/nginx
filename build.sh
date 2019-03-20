c0a28fb8b3ca9c5d826496cb349dc586605896d0
Allan de Queiroz <allandequeiroz@gmail.com>
2019-03-20 19:48:50 +0000

    Making it a bit flex

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+docker run --rm --privileged multiarch/qemu-user-static:register --reset
+
+docker build --build-arg SERVICE=ghost --build-arg PORT=2368 -t allandequeiroz/nginx:ghost-arm64 -f Dockerfile.arm64 .
+docker push allandequeiroz/nginx:ghost-arm64
+
+docker build --build-arg SERVICE=ghost --build-arg PORT=2368 -t allandequeiroz/nginx:ghost-amd64 -f Dockerfile.amd64 .
+docker push allandequeiroz/nginx:ghost-amd64
+
+docker build --build-arg SERVICE=wiki --build-arg PORT=8080 -t allandequeiroz/nginx:wiki-arm64 -f Dockerfile.arm64 .
+docker push allandequeiroz/nginx:wiki-arm64
+
+docker build --build-arg SERVICE=wiki --build-arg PORT=8080 -t allandequeiroz/nginx:wiki-amd64 -f Dockerfile.amd64 .
+docker push allandequeiroz/nginx:wiki-amd64
