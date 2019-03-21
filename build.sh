1b8ec3d6e8dca1fce68a3a32e1595cf0f93e45d0
Allan de Queiroz <allandequeiroz@gmail.com>
2019-03-21 04:50:22 +0000

    Fixing script

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ docker run --rm --privileged multiarch/qemu-user-static:register --reset
 #docker build --build-arg SERVICE=ghost --build-arg PORT=2368 -t allandequeiroz/nginx:ghost-arm64 -f Dockerfile.arm64 .
 #docker push allandequeiroz/nginx:ghost-arm64
 
-#docker rmi allandequeiroz/nginx:ghost-amd64
+docker rmi allandequeiroz/nginx:ghost-amd64
 docker build --build-arg SERVICE=ghost --build-arg PORT=2368 -t allandequeiroz/nginx:ghost-amd64 -f Dockerfile.amd64 .
 docker push allandequeiroz/nginx:ghost-amd64
 
@@ -14,6 +14,6 @@ docker push allandequeiroz/nginx:ghost-amd64
 #docker build --build-arg SERVICE=wiki --build-arg PORT=8080 -t allandequeiroz/nginx:wiki-arm64 -f Dockerfile.arm64 .
 #docker push allandequeiroz/nginx:wiki-arm64
 
-#docker rmi allandequeiroz/nginx:wiki-amd64
+docker rmi allandequeiroz/nginx:wiki-amd64
 docker build --build-arg SERVICE=wiki --build-arg PORT=8080 -t allandequeiroz/nginx:wiki-amd64 -f Dockerfile.amd64 .
 docker push allandequeiroz/nginx:wiki-amd64
